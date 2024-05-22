6bac78b8d4ddb6611324089e11307e1b91c4a7d3
yasminrp9 <yaspadilha665@gmail.com>
2024-05-22 18:45:38 -0300

    Classificador de xp

---
+++ index.js
@@ -0,0 +1,29 @@
+// Definindo a função para classificar o nível do herói
+function classificarHeroi(nome, xp) {
+  let nivel; // Variável para armazenar o nível do herói
+  
+  // Utilizando uma estrutura de decisão para determinar o nível com base na quantidade de experiência (XP)
+  if (xp < 1000) {
+      nivel = "Ferro";
+  } else if (xp >= 1001 && xp <= 2000) {
+      nivel = "Bronze";
+  } else if (xp >= 2001 && xp <= 5000) {
+      nivel = "Prata";
+  } else if (xp >= 5001 && xp <= 7000) {
+      nivel = "Ouro";
+  } else if (xp >= 7001 && xp <= 8000) {
+      nivel = "Platina";
+  } else if (xp >= 8001 && xp <= 9000) {
+      nivel = "Ascendente";
+  } else if (xp >= 9001 && xp <= 10000) {
+      nivel = "Imortal";
+  } else {
+      nivel = "Radiante";
+  }
+  
+  // Exibindo a mensagem com o nome e o nível do herói
+  console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
+
+// Exemplo de uso da função
+classificarHeroi("Yasmin", 10001);
